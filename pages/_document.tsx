fd945f2aa8813b2e7aadf187e279df244481c42d
Timéo SOETE <timeo.soete@etu.unistra.fr>
2025-06-17 16:01:45 +0200

    ajout favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="fr">
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/images/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        
+        {/* Apple Touch Icon - utilise le logo du portfolio */}
+        <link rel="apple-touch-icon" href="/images/logo_portfolio.png" />
+        
+        {/* Meta tags pour le site */}
+        <meta name="theme-color" content="#CDFB52" />
+        <meta name="description" content="Portfolio de Timéo Soëte - Développeur Web Frontend & Backend" />
+        <meta name="author" content="Timéo Soëte" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
